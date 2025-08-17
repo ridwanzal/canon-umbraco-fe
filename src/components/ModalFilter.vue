a56bfdab8396d801858939206c279060e89c3e84
M. Ridwan Zalbina <zalbinaridwan@gmail.com>
2025-08-17 22:08:14 +0700

    Update ModalFilter.vue

--- src/components/ModalFilter.vue
+++ src/components/ModalFilter.vue
@@ -2,7 +2,7 @@
   <div v-if="showModal" class="fixed inset-0 z-50 flex flex-col bg-white">
     <!-- Header -->
     <div class="flex items-center justify-between p-4 border-b border-gray-200">
-      <h2 class="text-xl font-bold">Filters</h2>
+      <h2 class="text-xl font-bold uppercase">Filters</h2>
       <button @click="$emit('close')"  class="text-gray-600 hover:text-gray-900 text-[45px] leading-[0]">
         &times;
       </button>
@@ -11,8 +11,8 @@
     <!-- Content -->
     <div class="flex-1 overflow-y-auto p-4">
       <!-- Accordion: Country -->
-      <div class="border-b border-gray-200">
-        <button class="w-full flex justify-between items-center py-3 text-lg font-medium"
+      <div class="relative">
+        <button class="border-b border-gray-200 w-full flex justify-between items-center py-3 text-lg font-medium"
           @click="toggleAccordion('country')">
           <span>Country</span>
           <svg class="w-5 h-5 text-gray-500 transition-transform duration-300"
@@ -21,7 +21,7 @@
             <path stroke-linecap="round" stroke-linejoin="round" d="M6 9l6 6 6-6" />
           </svg>
         </button>
-        <div v-if="openAccordion === 'country'" class="pl-4 pb-4 space-y-2">
+        <div v-if="openAccordion === 'country'" class="pl-4 pb-4 space-y-2 pt-4">
           <label class="flex items-center gap-2">
             <input type="checkbox" value="USA" v-model="selectedCountries" />
             USA
@@ -38,8 +38,8 @@
       </div>
 
       <!-- Accordion: Year -->
-      <div class="border-b border-gray-200 mb-5">
-        <button class="w-full flex justify-between items-center py-3 text-lg font-medium"
+      <div class="relative">
+        <button class="border-b border-gray-200 w-full flex justify-between items-center py-3 text-lg font-medium"
           @click="toggleAccordion('year')">
           <span>Year</span>
           <svg class="w-5 h-5 text-gray-500 transition-transform duration-300"
@@ -48,7 +48,7 @@
             <path stroke-linecap="round" stroke-linejoin="round" d="M6 9l6 6 6-6" />
           </svg>
         </button>
-        <div v-if="openAccordion === 'year'" class="pl-4 pb-4 space-y-2">
+        <div v-if="openAccordion === 'year'" class="pl-4 pb-4 space-y-2 pt-4">
           <label class="flex items-center gap-2">
             <input type="checkbox" value="2023" v-model="selectedYears" />
             2023
@@ -64,7 +64,7 @@
         </div>
       </div>
       <button
-        class="w-full py-3 bg-[#06AED8] text-white text-[14px] uppercase font-semibold rounded-full hover:bg-[#0599bf]"
+        class="mt-4 w-full py-3 bg-[#06AED8] text-white text-[14px] uppercase font-semibold rounded-full hover:bg-[#0599bf]"
         @click="applyFilter">
         Apply Filter
       </button>
